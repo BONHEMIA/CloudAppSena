5f6c18b459dfb06cd6d11fbc2f85147732c9f776
BONHEMIA <104398354+BONHEMIA@users.noreply.github.com>
2022-06-09 13:06:16 -0500

    Update AppController.java

--- src/main/java/com/georincon/escenarios/controller/AppController.java
+++ src/main/java/com/georincon/escenarios/controller/AppController.java
@@ -1,5 +1,5 @@
 package com.georincon.escenarios.controller;
-
+// Es bueno docuemntarlo todo para tener un mejor entendimiento del proyecto
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
